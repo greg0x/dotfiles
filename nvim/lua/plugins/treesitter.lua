c8fd46fb4dad7cdc0c66f8e8496c9bd73f3015b4
Greg Nagy <greg@dhamma.works>
2026-01-26 12:41:24 +0100

    Treesitter expansion for web and mobile development
    Adds parsers for TypeScript, JavaScript, HTML, CSS, Swift, and Kotlin.
    Includes treesitter-textobjects for function/class motions and
    nvim-ts-autotag for automatic HTML tag closing/renaming.

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -4,21 +4,38 @@ return {
     version = false,
     build = ":TSUpdate",
     event = { "BufReadPre", "BufNewFile" },
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter-textobjects",
+    },
     opts = {
       ensure_installed = {
+        -- Systems
         "rust",
         "go",
         "gomod",
         "gosum",
+        -- Web
+        "typescript",
+        "tsx",
+        "javascript",
+        "html",
+        "css",
+        "json",
+        "jsonc",
+        -- Mobile
+        "swift",
+        "kotlin",
+        -- Config
         "lua",
         "vim",
         "vimdoc",
-        "markdown",
-        "markdown_inline",
-        "json",
         "yaml",
         "toml",
         "bash",
+        -- Docs
+        "markdown",
+        "markdown_inline",
+        -- Git
         "diff",
         "gitcommit",
         "git_rebase",
@@ -34,9 +51,35 @@ return {
           node_decremental = "<bs>",
         },
       },
+      textobjects = {
+        select = {
+          enable = true,
+          lookahead = true,
+          keymaps = {
+            ["af"] = "@function.outer",
+            ["if"] = "@function.inner",
+            ["ac"] = "@class.outer",
+            ["ic"] = "@class.inner",
+            ["aa"] = "@parameter.outer",
+            ["ia"] = "@parameter.inner",
+          },
+        },
+        move = {
+          enable = true,
+          goto_next_start = { ["]f"] = "@function.outer", ["]c"] = "@class.outer" },
+          goto_prev_start = { ["[f"] = "@function.outer", ["[c"] = "@class.outer" },
+        },
+      },
     },
     config = function(_, opts)
       require("nvim-treesitter.configs").setup(opts)
     end,
+  },
+
+  -- Auto close/rename HTML tags
+  {
+    "windwp/nvim-ts-autotag",
+    event = { "BufReadPre", "BufNewFile" },
+    opts = {},
   },
 }
